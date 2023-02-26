7a71fff42410980ce57b2153c3132f91347f2eb9
ngqile <ngqile@gmail.com>
2023-02-26 16:08:08 +0800

    task  6

---
+++ src/main/java/com/strands/interview/task6/task6.java
@@ -0,0 +1,9 @@
+package com.strands.interview.task6;
+
+public class task6 {
+	String regex = "^[a-z]+\\.[a-z]+@strands\\.com$";
+	
+	String regex2 = "([A-Z]+)(\\d+\\/\\d+\\/\\d+)";
+	//([A-Z]+) for name
+	//(\d+\/\d+\/\d+) for date
+}
